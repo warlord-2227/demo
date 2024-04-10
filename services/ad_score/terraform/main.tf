27c9afb75e0c5192743a03207ecb85acaceeb68f
warlord-2227 <setu.iitb@gmail.com>
2024-04-10 16:36:11 +0530

    test_new commit

--- services/ad_score/terraform/main.tf
+++ services/ad_score/terraform/main.tf
@@ -1,5 +1,7 @@
 resource "google_storage_bucket" "bucket" {
-  name                        = "mast-${var.service}-meta-${var.environment}" # Every bucket name must be globally unique
+  #name                        = "mast-${var.service}-meta-${var.environment}" # Every bucket name must be globally unique
+  name = "mast-bucket-che-abc"
+  project = "my-project-6242-308916"
   location                    = "us-central1"
   storage_class               = "STANDARD"
   uniform_bucket_level_access = true
