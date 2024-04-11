a25782efcb43414fee061de1ed8987b76672877d
warlord-2227 <setu.iitb@gmail.com>
2024-04-11 17:14:39 +0530

    checking zip and deploying file

--- services/ad_score/terraform/main.tf
+++ services/ad_score/terraform/main.tf
@@ -5,7 +5,7 @@ variable "project_id" {
 
 variable "bucket_name" {
   description = "The name of the Google Cloud Storage bucket"
-  default     = "mast-bucket-che-abc-abc"  # Ensure this is globally unique
+  default     = "mast-bucket-che-abc-abcd"  # Ensure this is globally unique
 }
 
 variable "bucket_location" {
@@ -14,7 +14,7 @@ variable "bucket_location" {
 }
 
 locals {
-  source_code_dir  = "${path.module}/../src/functions"
+  source_code_dir  = "${path.module}/services/ad_score/src/functions"
   output_zip_path  = "${path.module}/function.zip"
   function_code_object_name = "functions"
 }
@@ -25,4 +25,16 @@ resource "google_storage_bucket" "bucket" {
   location                    = var.bucket_location
   storage_class               = "STANDARD"
   uniform_bucket_level_access = true
+}
+
+data "archive_file" "function_zip" {
+  type        = "zip"
+  source_dir  = local.source_code_dir
+  output_path = local.output_zip_path
+}
+
+resource "google_storage_bucket_object" "function_code" {
+  name   = local.function_code_object_name
+  bucket = google_storage_bucket.bucket.name
+  source = data.archive_file.function_zip.output_path
 }
