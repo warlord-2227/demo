da62e77a17665c936f104f98ef0322e94db0b49d
warlord-2227 <setu.iitb@gmail.com>
2024-04-11 22:40:52 +0530

    google_function source added

--- services/ad_score/src/functions/main.py
+++ services/ad_score/src/functions/main.py
@@ -1,19 +1,23 @@
-import base64
-import json
-import numpy as np
-import pandas as pd
-import pickle
-import joblib
-import sklearn
+
+import lib_main as training
+import predict as testing
+import pre_process
+import utilities
 from google.cloud import storage, pubsub_v1
 import google.cloud.logging
 import logging
-from io import BytesIO
-from ad_scoring import ad_scoring_model
-ad_model = ad_scoring_model()
-client = google.cloud.logging.Client(project="project")
-client.setup_logging()
+import json
+import base64
+import os 
 
-def hello_pubsub(cloud_event,context):
-    # Print out the data from Pub/Sub, to prove that it worked
-    ad_model.predicting_model(cloud_event)
+def Hello_pubsub(cloud_event,context):
+    logging.getLogger().setLevel(logging.INFO)
+    pubsub_message = base64.b64decode(cloud_event["data"]).decode('utf-8')
+    input_data = json.loads(pubsub_message)
+    logging.info(f"Keys in it {input_data.keys()}")
+    publisher = pubsub_v1.PublisherClient()
+    # result_topic = "projects/my-project-6242-308916/topics/pubsub-ad_score-meta-dev"
+    result_topic = os.getenv('RESULT_TOPIC')
+    json_output = {"abc":1}
+    future = publisher.publish(result_topic, data=json.dumps(json_output).encode('utf-8'))
+    
