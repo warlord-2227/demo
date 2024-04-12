635bb7792369e216af4b7538e8dec14f6fd437ef
warlord-2227 <setu.iitb@gmail.com>
2024-04-12 19:56:24 +0530

    test publisher msg

--- services/ad_score/src/functions/main.py
+++ services/ad_score/src/functions/main.py
@@ -11,13 +11,18 @@ import base64
 import os 
 
 def hello_pubsub(cloud_event,context):
-    logging.getLogger().setLevel(logging.INFO)
-    pubsub_message = base64.b64decode(cloud_event["data"]).decode('utf-8')
-    input_data = json.loads(pubsub_message)
-    logging.info(f"Keys in it {input_data.keys()}")
-    publisher = pubsub_v1.PublisherClient()
-    # result_topic = "projects/my-project-6242-308916/topics/pubsub-ad_score-meta-dev"
-    result_topic = os.getenv('RESULT_TOPIC')
-    json_output = {"abc":1}
-    future = publisher.publish(result_topic, data=json.dumps(json_output).encode('utf-8'))
-    
+    try:
+        logging.getLogger().setLevel(logging.INFO)
+        pubsub_message = base64.b64decode(cloud_event["data"]).decode('utf-8')
+        input_data = json.loads(pubsub_message)
+        logging.info(f"Keys in it {input_data.keys()}")
+        publisher = pubsub_v1.PublisherClient()
+        # result_topic = "projects/my-project-6242-308916/topics/pubsub-ad_score-meta-dev"
+        result_topic = os.getenv('RESULT_TOPIC')
+        json_output = {"abc":1}
+        future = publisher.publish(result_topic, data=json.dumps(json_output).encode('utf-8'))
+    except KeyError as e:
+        print(f"KeyError - missing key: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+           
